497dacc6785cb7d3d261971cbec9873f21463e98
sobhanniroula <sobhan_niroula@yahoo.com>
2019-06-12 22:30:36 +0300

    Rolled back app to its initial stage, again

---
+++ calcReactNative/index.js
@@ -0,0 +1,6 @@
+// This file is not in use at the moment..
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('SimpleCalculator', () => App);
