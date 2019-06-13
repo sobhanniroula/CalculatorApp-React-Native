513e963d04bd4c9e12a053fbd6acad3d5b849273
sobhanniroula <sobhan_niroula@yahoo.com>
2019-06-13 03:28:57 +0300

    Implemented everything, app now works successfully

--- calcReactNative/App.js
+++ calcReactNative/App.js
@@ -7,7 +7,8 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      resultText: '0'
+      resultText: '0',
+      calculationText: '0'
     };
 
     this.handleTouch = this.handleTouch.bind(this);
@@ -15,15 +16,25 @@ export default class App extends React.Component {
     this.calculateResult = this.calculateResult.bind(this);
   }
 
+
   calculateResult() {
     const text = this.state.resultText;
-    // parse
+    const opArr = ['/', '*', '-', '+', '.'];
+    const lastCharacter = this.state.resultText.split('').pop();
+
+    if (opArr.indexOf(lastCharacter) != -1) {
+      return;
+    } else {
+      this.setState({
+        calculationText: eval(text)
+      });
+    }
+    
   }
 
-  handleTouch(text) {
-    console.log(text);
 
-    if (text == 'DEL') {
+  handleTouch(text) {
+       if (text == 'DEL') {
       let orgText = this.state.resultText.split('');
       orgText.pop();
       if(orgText.length === 0) {
@@ -47,7 +58,6 @@ export default class App extends React.Component {
         });
       }
     }
-
   }
 
 
@@ -61,7 +71,11 @@ export default class App extends React.Component {
       case '*':
       case '-':
       case '+':
-        if (this.state.resultText === '0' || this.state.resultText === '') {
+        const opArr = ['/', '*', '-', '+'];
+        const lastCharacter = this.state.resultText.split('').pop();
+        if (opArr.indexOf(lastCharacter) != -1) {
+          return;
+        } else if (this.state.resultText === '0' || this.state.resultText === '') {
           return;
         } else {
           this.setState({
@@ -71,41 +85,49 @@ export default class App extends React.Component {
      }
   }
 
+
+  initialize() {
+    this.setState({
+      resultText: '0',
+      calculationText: '0'
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.displayResult}><Text style={styles.resultText}>{this.state.resultText}</Text></View>
-        <View style={styles.displayCalculation}><Text style={styles.calculationText}>Calculations</Text></View>
+        <View style={styles.displayCalculation}><Text style={styles.calculationText}>{this.state.calculationText}</Text></View>
         
         <View style={styles.buttons}>
           <View style={styles.numbers}>
             <View style={styles.row}>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('7')}><Text style={styles.btnText}>7</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('8')}><Text style={styles.btnText}>8</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('9')}><Text style={styles.btnText}>9</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="7" onPress={() => this.handleTouch('7')}><Text style={styles.btnText}>7</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="8" onPress={() => this.handleTouch('8')}><Text style={styles.btnText}>8</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="9" onPress={() => this.handleTouch('9')}><Text style={styles.btnText}>9</Text></TouchableOpacity>
             </View> 
             <View style={styles.row}>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('4')}><Text style={styles.btnText}>4</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('5')}><Text style={styles.btnText}>5</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('6')}><Text style={styles.btnText}>6</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="4" onPress={() => this.handleTouch('4')}><Text style={styles.btnText}>4</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="5" onPress={() => this.handleTouch('5')}><Text style={styles.btnText}>5</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="6" onPress={() => this.handleTouch('6')}><Text style={styles.btnText}>6</Text></TouchableOpacity>
             </View>  
             <View style={styles.row}>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('1')}><Text style={styles.btnText}>1</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('2')}><Text style={styles.btnText}>2</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('3')}><Text style={styles.btnText}>3</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="1" onPress={() => this.handleTouch('1')}><Text style={styles.btnText}>1</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="2" onPress={() => this.handleTouch('2')}><Text style={styles.btnText}>2</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="3" onPress={() => this.handleTouch('3')}><Text style={styles.btnText}>3</Text></TouchableOpacity>
             </View>  
             <View style={styles.row}>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('.')}><Text style={styles.btnText}>.</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('0')}><Text style={styles.btnText}>0</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn1} onPress={() => this.handleTouch('DEL')}><Text style={styles.btnText}>DEL</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="10" onPress={() => this.handleTouch('.')}><Text style={styles.btnText}>.</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="11" onPress={() => this.handleTouch('0')}><Text style={styles.btnText}>0</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn1} key="12" onPress={() => this.handleTouch('DEL')} onLongPress={() => this.initialize()}><Text style={styles.btnText}>DEL</Text></TouchableOpacity>
             </View>   
           </View>
           <View style={styles.operators}>
-              <TouchableOpacity style={styles.btn2} onPress={() => this.handleOperator('/')}><Text style={styles.btnText}>&divide;</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn2} onPress={() => this.handleOperator('*')}><Text style={styles.btnText}>&times;</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn2} onPress={() => this.handleOperator('-')}><Text style={styles.btnText}>-</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btn2} onPress={() => this.handleOperator('+')}><Text style={styles.btnText}>+</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.btnEquals} onPress={() => this.handleOperator('=')}><Text style={styles.btnText}>=</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn2} key="13" onPress={() => this.handleOperator('/')}><Text style={styles.btnText}>&divide;</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn2} key="14" onPress={() => this.handleOperator('*')}><Text style={styles.btnText}>&times;</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn2} key="15" onPress={() => this.handleOperator('-')}><Text style={styles.btnText}>-</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btn2} key="16" onPress={() => this.handleOperator('+')}><Text style={styles.btnText}>+</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.btnEquals} key="17" onPress={() => this.handleOperator('=')}><Text style={styles.btnText}>=</Text></TouchableOpacity>
           </View>
         </View>
       </View>
@@ -139,12 +161,12 @@ const styles = StyleSheet.create({
   },
 
   resultText: {
-    fontSize: 30,
+    fontSize: 35,
     color: '#fff'
   },
 
   calculationText: {
-    fontSize: 25,
+    fontSize: 30,
     color: '#333'
   },
 
